23ead8291414a1891a48fa7673c94b424b8d3361
Dhruv278 <dhruvgopani8@gmail.com>
2023-11-06 00:17:50 +0530

    'final'

---
+++ my-app/build/precache-manifest.07d5d569127f1420008140b7a2306b6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c695a9b4cd089b3bae79dc0c20399fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57d74007ce18d06445f9",
+    "url": "/static/css/main.3488b201.chunk.css"
+  },
+  {
+    "revision": "dd679810dbc343f11ba7",
+    "url": "/static/js/2.1f141c0f.chunk.js"
+  },
+  {
+    "revision": "07a2de58838b19b9f3a12d5f217d538c",
+    "url": "/static/js/2.1f141c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d74007ce18d06445f9",
+    "url": "/static/js/main.ba648b60.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
