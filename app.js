49470baaed57cb68ff1814d6c72e87f16afee6b0
Dhruv278 <dhruvgopani8@gmail.com>
2023-11-06 00:14:19 +0530

    'final'

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ app.use('/api/v1/post',PostRoute);
 
 if(process.env.NODE_ENV==='production'){
    
-    app.use(express.static('my-app/build'));
+    app.use(express.static(path.join(__dirname,'my-app/build')))
     
     app.use('*',(req,res)=>{
       
-       res.sendFile(path.resolve(__dirname,'my-app','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'my-app/build/index.html'))
     //    res.send('hello')/
     })
 
